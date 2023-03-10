5d8a275494680f60c14d779c2cebacddf80d3bc7
陈文 <1204456064@qq.com>
2023-03-10 14:00:42 +0800

    feat: 基础配置提交

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Button } from 'antd'
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>我是无敌的antd啊</Button>
+    </div>
+  );
+}
+
+export default App;
